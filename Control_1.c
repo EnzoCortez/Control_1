a67971d71201718c74d25a1a37d300ae346428b2
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-24 15:10:47 -0500

    Se muestra el resultado en pantalla y se corrigue el error de impresion

--- Control_1.c
+++ Control_1.c
@@ -10,22 +10,28 @@ Salida: 14
 En este ejemplo el número que interrumpe la lectura es el 0*/
 
 /*Se define la funcion suma*/
-
-
 int suma(int a,int b){
-    b=0;
-    a=0;
-    
-    while (a>0)
-    {
-    scanf("%d",&a); 
-    b=b+a;
-    return b;
-    }  
+    return a+b;
 }
 
-int main(int a,int b){
-    printf("Ingrese la serie de numeros enteros");
-    scanf("%d",&a);
-    printf("El resultado de su serie es;", suma(a,b));
+/*Creacion del programa principal*/
+int main(){
+    int num,total=0;
+    printf("Ingrese un número entero (0 para salir): ");
+    do { /* Calculo y solicitud de datos */
+        printf("Ingrese un número entero (0 para salir): ");
+        scanf("%d", &num);
+        total = suma(total, num);
+
+    } while (num != 0);
+    printf("La suma total es: %d\n", total);
+    return 0;
 }
+
+
+
+
+
+
+
+
