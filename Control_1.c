953b6c92652c5de7a00c60d8c67f8b65b68dee68
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-10-24 14:31:21 -0500

    Se construye la funcion princial del programa y la funcion suma para calcular el resultado de la serie numerica

---
+++ Control_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*Utilizando el concepto de función, construya un programa que calcule la suma de una serie de números enteros leídos del teclado. 
+El programa debe detenerse cuando el usuario ingrese el número 0.
+
+Por ejemplo:
+Entrada: 1 5 3 5 0
+Salida: 14
+
+En este ejemplo el número que interrumpe la lectura es el 0*/
+
+/*Se define la funcion suma*/
+int a=0;
+int suma(a){
+    scanf("Ingrese la serie de numeros enteros");
+
+}
+
+int main(){
+    suma(a);
+}
